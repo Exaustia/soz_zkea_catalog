9342143404451133ab779c06bf40dd0cfbcd490d
Exaustia <Ferrer.benoit@orange.fr>
2024-08-08 18:47:53 +0200

    Update MenuCategory.tsx

--- src/app/components/MenuCategory.tsx
+++ src/app/components/MenuCategory.tsx
@@ -18,10 +18,13 @@ const MenuCategory = () => {
     }
   };
 
-
   return (
     <div className="relative" id={"cat_ctn"}>
-      <div className="overflow-auto flex gap-8 p-4" ref={container} style={{ scrollBehavior: "smooth" }}>
+      <div
+        className="overflow-auto flex gap-8 p-4"
+        ref={container}
+        style={{ scrollBehavior: "smooth" }}
+      >
         <button
           onClick={() => handleScroll("left")}
           className="absolute left-0 h-7 w-7 bg-gray-900 top-0 bottom-0 m-auto rounded-full opacity-0 flex items-center justify-center transition-all"
@@ -34,8 +37,14 @@ const MenuCategory = () => {
             key={item.slug}
             className="flex flex-col justify-center items-center min-w-24 gap-4 cursor-pointer menu_category"
           >
-            <Image src={item.image} alt="cat-img" width={80} height={80} />
-            <span className="text-black text-sm text-center">{item.name}</span>
+            <div className="h-2/3">
+              <Image src={item.image} alt="cat-img" width={80} height={80} />
+            </div>
+            <div className="h-1/3 justify-center flex items-center">
+              <span className="text-black text-sm text-center">
+                {item.name}
+              </span>
+            </div>
           </Link>
         ))}
         <button
