4ac169e834a4f92a30a90bef2de033618a78f430
Exaustia <Ferrer.benoit@orange.fr>
2024-08-09 12:15:11 +0200

    Hover on warning product

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { useMenu } from "../context/MenuProvider";
 import getUrl from "@/utils/url";
 import { findSlug } from "@/utils/findType";
+import classNames from "classnames";
 
 /* eslint-disable @next/next/no-img-element */
 
@@ -20,9 +21,9 @@ export interface productInterface {
 
 const ProductCard = ({ product }: { product: productInterface }) => {
   const { toggleView } = useMenu();
+  const [isHover, setIsHover] = useState(false);
   const [imageError, setImageError] = useState(false);
 
-  const slug = findSlug(product.type);
   return (
     <div
       className={
@@ -32,9 +33,7 @@ const ProductCard = ({ product }: { product: productInterface }) => {
       <div className="relative w-full h-3/4">
         <Image
           src={
-            !imageError
-              ? getUrl(product.model, "image")
-              : "/images/noimage.jpg"
+            !imageError ? getUrl(product.model, "image") : "/images/noimage.jpg"
           }
           unoptimized
           fill
@@ -50,17 +49,40 @@ const ProductCard = ({ product }: { product: productInterface }) => {
         ></div>
       </div>
 
-      <div className="p-4 flex gap-2 justify-between items-center h-1/4 bg-blackFrame opacity-75 ">
+      <div className="p-4 flex gap-2 justify-between items-center h-1/4 bg-blackFrame opacity-75 cursor-default ">
         <div className="flex flex-col">
-          <span className="text-sm font-semibold text-white">{product.name}</span>
+          <span className="text-sm font-semibold text-white">
+            {product.name}
+          </span>
           <span className="text-sm font-semibold text-secondary">
             {product.price}$
           </span>
         </div>
-        <div className="flex flex-col">
-          <span className="text-sm font-semibold">
+        <div className="flex flex-col relative cursor-pointer">
+          <button
+            data-tooltip-target="tooltip-default"
+            type="button"
+            className="text-sm font-semibold cursor-pointer"
+            onMouseOver={() => {
+              setIsHover(!isHover);
+            }}
+            onMouseLeave={() => {
+              setIsHover(false);
+            }}
+          >
             {!product.collision && "⚠️"}
-          </span>
+          </button>
+          <div
+            id="tooltip-default"
+            role="tooltip"
+            className={classNames(
+              { hidden: !isHover },
+              "absolute w-28 left-[-110px] top-[-30px] z-50 p-1 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg text-center"
+            )}
+          >
+            Pas de collision
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
         </div>
       </div>
     </div>
