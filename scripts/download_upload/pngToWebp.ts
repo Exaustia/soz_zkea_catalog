d38737f1b9d6b63c175a6188a6b72336e1b7ca71
Exaustia <Ferrer.benoit@orange.fr>
2024-08-19 19:51:26 +0200

    convert png to webp

---
+++ scripts/download_upload/pngToWebp.ts
@@ -0,0 +1,41 @@
+import sharp from "sharp";
+import { config } from "dotenv";
+import fs from "fs";
+
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getAWSImgs, getAWSModels } from "./utils";
+
+config();
+
+async function readPngToBuffer(filePath: string): Promise<Buffer> {
+  return fs.promises.readFile(filePath);
+}
+
+// Fonction pour convertir une image en WebP
+async function convertToWebP() {
+  const localFolder = "../allImagesPNG";
+
+  // Lire tous les fichiers du dossier local
+  const files = fs.readdirSync(localFolder);
+
+  for (const file of files) {
+    const filePath = `${localFolder}/${file}`;
+    const fileStream = await readPngToBuffer(filePath);
+
+    try {
+      // Convertir l'image en WebP
+      const webpBuffer = await sharp(fileStream).webp().toBuffer();
+
+      // Sauvegarder le fichier converti localement (optionnel)
+      const outputFilePath = `../webp/${file.replace(/\.[^/.]+$/, ".webp")}`;
+      fs.writeFileSync(outputFilePath, webpBuffer);
+
+      console.log(`Image convertie : ${file} -> ${outputFilePath}`);
+    } catch (error) {
+      console.error(`Erreur lors de la conversion de ${file} :`, error);
+    }
+  }
+}
+
+// Lancer le script
+convertToWebP();
--- zkea_website/src/utils/url.ts
+++ zkea_website/src/utils/url.ts
@@ -3,7 +3,7 @@ const images = "https://cdn.exaustia.com/soz/catalog/assets/images/";
 
 const getUrl = (name: string, type: string): string => {
   if (type === "glb") return `${models}${name}.glb`;
-  else return `${images}${name}.png`;
+  else return `${images}${name}.webp`;
 };
 
 export default getUrl;
